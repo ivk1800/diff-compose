9d0cc496f3fe3d696c3b507b843ecc772a68cf5b
Ivan <ivan@ivk1800.ru>
2023-01-05 19:57:17 +0400

    Add WindowKeyEventDelegate

--- src/jvmMain/kotlin/ru/ivk1800/diff/feature/repositoryview/ui/RepositoryViewWindow.kt
+++ src/jvmMain/kotlin/ru/ivk1800/diff/feature/repositoryview/ui/RepositoryViewWindow.kt
@@ -15,14 +15,19 @@ import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberWindowState
 import ru.ivk1800.diff.feature.repositoryview.di.compose.LocalRepositoryViewWindowScope
 import ru.ivk1800.diff.feature.repositoryview.presentation.RepositoryViewEvent
-import ru.ivk1800.diff.presentation.viewModel
 import ru.ivk1800.diff.ui.compose.DiffAlertDialog
+import ru.ivk1800.diff.ui.compose.rememberWindowKeyEventDelegate
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun RepositoryViewWindow() {
     val scope = LocalRepositoryViewWindowScope.current
-    val viewModel = viewModel { scope.repositoryViewViewModel }
+    val viewModel = scope.repositoryViewViewModel
+
+    val keyEventDelegate = rememberWindowKeyEventDelegate(
+        test = { event -> event.isMetaPressed && event.key == Key.R },
+        onDown = { viewModel.onEvent(RepositoryViewEvent.OnReload) },
+    )
 
     Window(
         title = scope.id.path,
@@ -31,14 +36,7 @@ fun RepositoryViewWindow() {
             size = DpSize(width = 1280.dp, height = 720.dp)
         ),
         onCloseRequest = { scope.router.close(scope.id) },
-        onKeyEvent = { event ->
-            if (event.isMetaPressed && event.key == Key.R) {
-                viewModel.onEvent(RepositoryViewEvent.OnReload)
-                true
-            } else {
-                false
-            }
-        }
+        onKeyEvent = keyEventDelegate::onKeyEvent,
     ) {
 
         val state by viewModel.state.collectAsState()
---
+++ src/jvmMain/kotlin/ru/ivk1800/diff/ui/compose/WindowKeyEventDelegate.kt
@@ -0,0 +1,66 @@
+package ru.ivk1800.diff.ui.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.type
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class WindowKeyEventDelegate(
+    private val test: (KeyEvent) -> Boolean,
+    private val onDown: () -> Unit,
+) {
+
+    // TODO: add main dispatcher
+    private val scope: CoroutineScope = CoroutineScope(SupervisorJob())
+
+    private val keyEventChannel = Channel<KeyEventType>()
+
+    init {
+        keyEventChannel.consumeAsFlow()
+            .filter { it == KeyEventType.KeyUp || it == KeyEventType.KeyDown }
+            .distinctUntilChanged()
+            .filter { it == KeyEventType.KeyDown }
+            .onEach { onDown.invoke() }
+            .launchIn(scope)
+    }
+
+    fun onKeyEvent(event: KeyEvent): Boolean {
+        return if (test.invoke(event)) {
+            scope.launch { keyEventChannel.send(event.type) }
+            true
+        } else {
+            false
+        }
+    }
+
+    fun dispose() {
+        scope.cancel()
+        keyEventChannel.close()
+    }
+}
+
+@Composable
+fun rememberWindowKeyEventDelegate(
+    test: (KeyEvent) -> Boolean,
+    onDown: () -> Unit,
+): WindowKeyEventDelegate {
+    val delegate = remember { WindowKeyEventDelegate(test, onDown) }
+
+    DisposableEffect(key1 = delegate) {
+        onDispose { delegate.dispose() }
+    }
+
+    return delegate
+}
