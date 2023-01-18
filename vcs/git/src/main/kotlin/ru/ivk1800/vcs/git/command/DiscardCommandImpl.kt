1ea353df7f3881d22df266d5d3c8ba9203386726
Ivan <ivan@ivk1800.ru>
2023-01-18 19:52:50 +0400

    implement DiscardCommand

--- vcs/git/src/main/kotlin/ru/ivk1800/vcs/git/command/DiscardCommandImpl.kt
+++ vcs/git/src/main/kotlin/ru/ivk1800/vcs/git/command/DiscardCommandImpl.kt
@@ -2,12 +2,16 @@ package ru.ivk1800.vcs.git.command
 
 import ru.ivk1800.vcs.api.command.DiscardCommand
 import java.nio.file.Path
+import kotlin.io.path.exists
+import kotlin.io.path.writeText
 
 internal class DiscardCommandImpl(
     private val directory: Path,
     options: Options,
 ) : DiscardCommand(options) {
     override suspend fun run() {
-        TODO("Not yet implemented")
+        val filePath = Path.of(directory.toString(), options.fileName)
+        check(filePath.exists()) { "Unable discard changes, file is not exists: ${options.fileName}" }
+        filePath.writeText(options.content)
     }
 }
